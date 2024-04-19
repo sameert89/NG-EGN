53b57ce5ce8ad59cee534079600b728ea2b48819
X6STRIVE <X6STRIVE@IN05N001NS>
2024-04-19 20:20:11 +0530

    cc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
+
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'eventgridmqttangular.southindia-1.ts.eventgrid.azure.net',
+  port: 443,
+  username: 'client1-authn-ID',
+  path: '/mqtt',
+  protocol: 'ws',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS)),
+  ],
 };
--- src/app/components/display-mqtt-message/display-mqtt-message.component.ts
+++ src/app/components/display-mqtt-message/display-mqtt-message.component.ts
@@ -1,6 +1,8 @@
 import * as mqtt from 'mqtt/dist/mqtt';
+import { IMqttMessage, IMqttServiceOptions, MqttModule, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-display-mqtt-message',
@@ -9,23 +11,18 @@ import { Component } from '@angular/core';
   templateUrl: './display-mqtt-message.component.html',
   styleUrl: './display-mqtt-message.component.css',
 })
-export class DisplayMqttMessageComponent {
-  options: any = {
-    port: 8883,
-    username: 'localhost',
-    clientId: 'angularclient',
-    path: '/mqtt',
-  };
-  client: any;
-  constructor() {
-    try {
-      this.client = mqtt.connect(
-        'ws://eventgridmqttangular.southindia-1.ts.eventgrid.azure.net',
-        this.options
-      );
-    } catch (error) {
-      console.log(error);
-    }
+export class DisplayMqttMessageComponent implements OnDestroy {
+  private subscription: Subscription;
+  public message: string = '';
+  constructor(private readonly mqttService: MqttService) {
+    this.subscription = this.mqttService
+      .observe('my/topic')
+      .subscribe((message: IMqttMessage) => {
+        this.message = message.payload.toString();
+      });
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
 /*
