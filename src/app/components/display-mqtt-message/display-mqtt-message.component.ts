0bc905d7c541e55f089d05de28511246da347238
sameert89 <91889637+sameert89@users.noreply.github.com>
2024-04-22 23:20:14 +0530

    fc

--- src/app/components/display-mqtt-message/display-mqtt-message.component.ts
+++ src/app/components/display-mqtt-message/display-mqtt-message.component.ts
@@ -1,4 +1,7 @@
-import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { Buffer } from 'buffer';
+import mqtt from 'mqtt';
+import { MqttClient } from 'mqtt/dist/mqtt';
+import { IMqttMessage, IMqttServiceOptions, MqttService } from 'ngx-mqtt';
 import { Subscription } from 'rxjs';
 
 // src/app/display-mqtt-message/display-mqtt-message.component.ts
@@ -15,31 +18,32 @@ export class DisplayMqttMessageComponent implements OnDestroy {
   private subscription?: Subscription;
   private connectionSubscription?: Subscription;
   public message: string = '';
+  public MQTT_SERVICE_OPTIONS: any = {
+    hostname: 'eventgridmqttangular.southindia-1.ts.eventgrid.azure.net',
+    port: 443,
+    path: '/mqtt',
+    protocol: 'wss',
+    username: 'angularapp',
+    protocolVersion: 5,
+    properties: {
+      authenticationMethod: 'OAUTH2-JWT',
+      authenticationData: Buffer.from(
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSIsImtpZCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSJ9.eyJhdWQiOiJodHRwczovL2V2ZW50Z3JpZC5henVyZS5uZXQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84OGRiODQyMC1jMWU5LTRjYzQtYjVlNC00ZjRlMzI0NTlhN2QvIiwiaWF0IjoxNzEzODA3MzE1LCJuYmYiOjE3MTM4MDczMTUsImV4cCI6MTcxMzgxMTIxNSwiYWlvIjoiRTJOZ1lMZ1VXeHRsL3lqNjdlRWUzaDkvUHVYRkFRQT0iLCJhcHBpZCI6ImZmNzFiNDBlLWU3ODEtNGU5Yy1iNzE3LWEzYzUwYmVlZTRiNSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0Lzg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZC8iLCJvaWQiOiI3YWVkMGY1Yi1lZWExLTRlOWItYTI1Ni1mYjM5YTcwOGVlZTAiLCJyaCI6IjAuQVNzQUlJVGJpT25CeEV5MTVFOU9Na1dhZlhnS1BJTGdYVVZFcF9YQzlDMTl5SnZDQUFBLiIsInN1YiI6IjdhZWQwZjViLWVlYTEtNGU5Yi1hMjU2LWZiMzlhNzA4ZWVlMCIsInRpZCI6Ijg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZCIsInV0aSI6InBTNERGaF83NUVHcVRwb1RCbk5YQUEiLCJ2ZXIiOiIxLjAifQ.o2okbzBjQzqbKzPSrNwG6RjJJjHW3Q0YfMhYQmoMSXZLMIo3tlFpEMw0LHPG3ZUM6E_3Ob-e-j-D-IEt_KFEkPBxfAElUWP5F5Y5JGBjBYZ51eHdf2CR-HRhK_QBNRC0-MsaUYs9nZlKPF33sxYLzbEPQYLv0icWat1HJpg6HbXG7w9aTtlIvk3etoj2qAF5cO4ZDyi3YGB8R6kk_iiVvb-VEO9byZrdUAj8ToreodM6hlYAtFKO1DcYKsxrV6mOfJwE0yW-sUNSik24G11RGiq0eClvyNqh95dUvgyUux4tdawK1lgmRCmpM9jC9InzDBDDqaHgeTzR3-mDa_LKUw'
+      ),
+    },
+  };
+  client?: MqttClient;
 
-  constructor(private readonly mqttService: MqttService) {
-    // Subscribe to check when the MQTT client successfully connects to the
-    // broker
-    console.log('I ran');
-    this.connectionSubscription = this.mqttService.onConnect.subscribe(() => {
-      console.log('Connected to MQTT broker successfully');
-    });
-
-    // Subscribe to listen for messages on a specific topic
-    this.subscription = this.mqttService
-      .observe('angularmqttdemo/topic1')
-      .subscribe((message: IMqttMessage) => {
-        console.log('New message received');
-        this.message = message.payload.toString(); // Converting message payload to string
-      });
-
-    // Optional: Subscribe to connection error events
-    this.mqttService.onError.subscribe((error) => {
-      console.log('Connection to MQTT broker failed:', error);
-    });
-  }
-
+  constructor() {}
   ngOnDestroy() {
     this.subscription?.unsubscribe();
     this.connectionSubscription?.unsubscribe();
+  }
+  async connectMqtt() {
+    this.client = await mqtt.connectAsync(this.MQTT_SERVICE_OPTIONS);
+    console.log(this.client.connected);
+  }
+  async publishMsg() {
+    this.client?.publishAsync('angularmqttdemo/topic1', 'Hello World');
   }
 }
