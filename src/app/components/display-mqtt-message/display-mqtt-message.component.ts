a158f972f3b6b3900acbeb6ea53a262719afaa22
sameert89 <91889637+sameert89@users.noreply.github.com>
2024-04-22 21:46:36 +0530

    zyayz

--- src/app/components/display-mqtt-message/display-mqtt-message.component.ts
+++ src/app/components/display-mqtt-message/display-mqtt-message.component.ts
@@ -1,11 +1,7 @@
-import {
-  IMqttMessage,
-  IMqttServiceOptions,
-  MqttModule,
-  MqttService,
-} from 'ngx-mqtt';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { Subscription } from 'rxjs';
 
+// src/app/display-mqtt-message/display-mqtt-message.component.ts
 import { Component, OnDestroy } from '@angular/core';
 
 @Component({
@@ -13,27 +9,37 @@ import { Component, OnDestroy } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './display-mqtt-message.component.html',
-  styleUrl: './display-mqtt-message.component.css',
+  styleUrls: ['./display-mqtt-message.component.css'],
 })
 export class DisplayMqttMessageComponent implements OnDestroy {
   private subscription?: Subscription;
+  private connectionSubscription?: Subscription;
   public message: string = '';
+
   constructor(private readonly mqttService: MqttService) {
+    // Subscribe to check when the MQTT client successfully connects to the
+    // broker
+    console.log('I ran');
+    this.connectionSubscription = this.mqttService.onConnect.subscribe(() => {
+      console.log('Connected to MQTT broker successfully');
+    });
+
+    // Subscribe to listen for messages on a specific topic
     this.subscription = this.mqttService
       .observe('angularmqttdemo/topic1')
       .subscribe((message: IMqttMessage) => {
-        console.log('henlo');
-        this.message = 'Success';
-        // this.message = message.payload.toString();
+        console.log('New message received');
+        this.message = message.payload.toString(); // Converting message payload to string
       });
+
+    // Optional: Subscribe to connection error events
+    this.mqttService.onError.subscribe((error) => {
+      console.log('Connection to MQTT broker failed:', error);
+    });
   }
+
   ngOnDestroy() {
     this.subscription?.unsubscribe();
+    this.connectionSubscription?.unsubscribe();
   }
 }
-/*
-hostname: 'eventgridmqttangular.southindia-1.ts.eventgrid.azure.net',
-  port: 8883,
-  username: 'localhost',
-  clientId: 'angularclient',
-*/
