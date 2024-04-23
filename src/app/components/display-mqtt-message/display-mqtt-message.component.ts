5e13065c7a9d9dd735ca9038f562254da5eafab0
X6STRIVE <X6STRIVE@IN05N001NS>
2024-04-23 17:12:13 +0530

    auto post

--- src/app/components/display-mqtt-message/display-mqtt-message.component.ts
+++ src/app/components/display-mqtt-message/display-mqtt-message.component.ts
@@ -29,6 +29,8 @@ export class DisplayMqttMessageComponent implements OnDestroy {
     protocolVersion: 5,
     properties: {
       authenticationMethod: 'OAUTH2-JWT',
+      authenticationData:
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSIsImtpZCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSJ9.eyJhdWQiOiJodHRwczovL2V2ZW50Z3JpZC5henVyZS5uZXQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84OGRiODQyMC1jMWU5LTRjYzQtYjVlNC00ZjRlMzI0NTlhN2QvIiwiaWF0IjoxNzEzODcxMzY1LCJuYmYiOjE3MTM4NzEzNjUsImV4cCI6MTcxMzg3NTI2NSwiYWlvIjoiRTJOZ1lQQXZuVk5nMUJKd3pDbGhheFhQMlkvL0FRPT0iLCJhcHBpZCI6ImZmNzFiNDBlLWU3ODEtNGU5Yy1iNzE3LWEzYzUwYmVlZTRiNSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0Lzg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZC8iLCJvaWQiOiI3YWVkMGY1Yi1lZWExLTRlOWItYTI1Ni1mYjM5YTcwOGVlZTAiLCJyaCI6IjAuQVNzQUlJVGJpT25CeEV5MTVFOU9Na1dhZlhnS1BJTGdYVVZFcF9YQzlDMTl5SnZDQUFBLiIsInN1YiI6IjdhZWQwZjViLWVlYTEtNGU5Yi1hMjU2LWZiMzlhNzA4ZWVlMCIsInRpZCI6Ijg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZCIsInV0aSI6ImVvazdlc29JckV5VTJwU1ROSGFKQUEiLCJ2ZXIiOiIxLjAifQ.RQY41LFE4bY0yot3nI_v6BIGAEdYhl7euCyHi9Po_F8xY20H60MQxF2HUUyVO1B1YDPUPOM_b56GM1dhzeOxdB1s7DW70clz0KCl0wy4rfxQtmENKLKZn57wrwZ3yYBxE0SfGCNLAl5ity3bPv9GAFokHAaAQcpzaJ6oTi-Z5cKcXXLU4MbCk7oj2KbcQrmUYe2Bq_hB7lUQ9laxYzpp8BfcHPcUoStPb-yihX57VxqygMBIvEf7uNSOkH7KQPjCfBhtTVCCS-TmV8BzDbBIyydLvptld1lKU48qMyIsZliD0VgLT2L-zYRLXM7fr1n94fw2eJ2340ys0oJamm8CWw',
     },
   };
   public PUBLIC_HOST: any = {
@@ -38,24 +40,24 @@ export class DisplayMqttMessageComponent implements OnDestroy {
   };
   client?: MqttClient;
   constructor(private http: HttpClient) {
-    http
-      .post(
-        'https://login.microsoftonline.com/88db8420-c1e9-4cc4-b5e4-4f4e32459a7d/oauth2/v2.0/token',
-        {
-          client_id: 'ff71b40e-e781-4e9c-b717-a3c50beee4b5',
-          client_secret: 'byI8Q~BOk8_UEQyuPGJHQqQG29G4vjcubtZGvc3V',
-          scope: 'https://eventgrid.azure.net/.default',
-          grant_type: 'client_credentials',
-        }
-      )
-      .subscribe({
-        next: (response: any) => {
-          console.log(response);
-          this.MQTT_SERVICE_OPTIONS.properties.authenticationData = Buffer.from(
-            response.access_token
-          );
-        },
-      });
+    // http
+    //   .post(
+    //     'https://login.microsoftonline.com/88db8420-c1e9-4cc4-b5e4-4f4e32459a7d/oauth2/v2.0/token',
+    //     {
+    //       client_id: 'ff71b40e-e781-4e9c-b717-a3c50beee4b5',
+    //       client_secret: 'byI8Q~BOk8_UEQyuPGJHQqQG29G4vjcubtZGvc3V',
+    //       scope: 'https://eventgrid.azure.net/.default',
+    //       grant_type: 'client_credentials',
+    //     }
+    //   )
+    //   .subscribe({
+    //     next: (response: any) => {
+    //       console.log(response);
+    //       this.MQTT_SERVICE_OPTIONS.properties.authenticationData = Buffer.from(
+    //         response.access_token
+    //       );
+    //     },
+    //   });
   }
   ngOnDestroy() {
     this.subscription?.unsubscribe();
