23b7937026c65f7d1a7712ae9ab3cdc67785b5da
sameert89 <91889637+sameert89@users.noreply.github.com>
2024-04-23 23:18:10 +0530

    iframe for auto cert

--- src/app/components/display-mqtt-message/display-mqtt-message.component.ts
+++ src/app/components/display-mqtt-message/display-mqtt-message.component.ts
@@ -4,9 +4,14 @@ import { MqttClient } from 'mqtt/dist/mqtt';
 import { IMqttMessage, IMqttServiceOptions, MqttService } from 'ngx-mqtt';
 import { Subscription } from 'rxjs';
 
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHandler,
+} from '@angular/common/http';
 // src/app/display-mqtt-message/display-mqtt-message.component.ts
 import { Component, importProvidersFrom, OnDestroy } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-display-mqtt-message',
@@ -16,7 +21,7 @@ import { Component, importProvidersFrom, OnDestroy } from '@angular/core';
   styleUrls: ['./display-mqtt-message.component.css'],
   providers: [],
 })
-export class DisplayMqttMessageComponent{
+export class DisplayMqttMessageComponent {
   private subscription?: Subscription;
   private connectionSubscription?: Subscription;
   public message: string = 'Not Connected';
@@ -29,8 +34,10 @@ export class DisplayMqttMessageComponent{
     protocolVersion: 5,
     properties: {
       authenticationMethod: 'OAUTH2-JWT',
-      authenticationData: Buffer.from('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSIsImtpZCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSJ9.eyJhdWQiOiJodHRwczovL2V2ZW50Z3JpZC5henVyZS5uZXQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84OGRiODQyMC1jMWU5LTRjYzQtYjVlNC00ZjRlMzI0NTlhN2QvIiwiaWF0IjoxNzEzODkyMDUxLCJuYmYiOjE3MTM4OTIwNTEsImV4cCI6MTcxMzg5NTk1MSwiYWlvIjoiRTJOZ1lOaTN2NjkvZS9qTWxhS3pOOFowZkFnVEJBQT0iLCJhcHBpZCI6ImZmNzFiNDBlLWU3ODEtNGU5Yy1iNzE3LWEzYzUwYmVlZTRiNSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0Lzg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZC8iLCJvaWQiOiI3YWVkMGY1Yi1lZWExLTRlOWItYTI1Ni1mYjM5YTcwOGVlZTAiLCJyaCI6IjAuQVNzQUlJVGJpT25CeEV5MTVFOU9Na1dhZlhnS1BJTGdYVVZFcF9YQzlDMTl5SnZDQUFBLiIsInN1YiI6IjdhZWQwZjViLWVlYTEtNGU5Yi1hMjU2LWZiMzlhNzA4ZWVlMCIsInRpZCI6Ijg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZCIsInV0aSI6Ik9OQmRjdjcxREVPamlrNGoxRHh0QUEiLCJ2ZXIiOiIxLjAifQ.3ekJorTvz2ErRmQW-NuAZNLDQtEqqp61eetH921tI1DUZtBpjPIr7lLiD7vaLbpX888PEp4RF4_0m_H2EEtRLkiBDHEVLO_ChVlB-2lxXqqZSJgvVL6lfDv1XpoVu_DSosI_0shflDoqtf3AMmPF13MkwuZU2P4n4CVL7wNzlkUzf4Fza5_IBVqcyG71rmybY5YUWRDAi2x3Xz-rG-MHHdM1nhYl9v60mu6UyUhGcpDLg05zjpu4Z5J_Vn4tnWtyNVnmzTUWbpOy4iDN0Z8aBIryK2z_eR75EHg_VYOVPmeTVNFNm-An0vFGKmDivfYTn3q8KuAAtoF-HoK2eluRMg')
-    }
+      authenticationData: Buffer.from(
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSIsImtpZCI6InEtMjNmYWxldlpoaEQzaG05Q1Fia1A1TVF5VSJ9.eyJhdWQiOiJodHRwczovL2V2ZW50Z3JpZC5henVyZS5uZXQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84OGRiODQyMC1jMWU5LTRjYzQtYjVlNC00ZjRlMzI0NTlhN2QvIiwiaWF0IjoxNzEzODkyMDUxLCJuYmYiOjE3MTM4OTIwNTEsImV4cCI6MTcxMzg5NTk1MSwiYWlvIjoiRTJOZ1lOaTN2NjkvZS9qTWxhS3pOOFowZkFnVEJBQT0iLCJhcHBpZCI6ImZmNzFiNDBlLWU3ODEtNGU5Yy1iNzE3LWEzYzUwYmVlZTRiNSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0Lzg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZC8iLCJvaWQiOiI3YWVkMGY1Yi1lZWExLTRlOWItYTI1Ni1mYjM5YTcwOGVlZTAiLCJyaCI6IjAuQVNzQUlJVGJpT25CeEV5MTVFOU9Na1dhZlhnS1BJTGdYVVZFcF9YQzlDMTl5SnZDQUFBLiIsInN1YiI6IjdhZWQwZjViLWVlYTEtNGU5Yi1hMjU2LWZiMzlhNzA4ZWVlMCIsInRpZCI6Ijg4ZGI4NDIwLWMxZTktNGNjNC1iNWU0LTRmNGUzMjQ1OWE3ZCIsInV0aSI6Ik9OQmRjdjcxREVPamlrNGoxRHh0QUEiLCJ2ZXIiOiIxLjAifQ.3ekJorTvz2ErRmQW-NuAZNLDQtEqqp61eetH921tI1DUZtBpjPIr7lLiD7vaLbpX888PEp4RF4_0m_H2EEtRLkiBDHEVLO_ChVlB-2lxXqqZSJgvVL6lfDv1XpoVu_DSosI_0shflDoqtf3AMmPF13MkwuZU2P4n4CVL7wNzlkUzf4Fza5_IBVqcyG71rmybY5YUWRDAi2x3Xz-rG-MHHdM1nhYl9v60mu6UyUhGcpDLg05zjpu4Z5J_Vn4tnWtyNVnmzTUWbpOy4iDN0Z8aBIryK2z_eR75EHg_VYOVPmeTVNFNm-An0vFGKmDivfYTn3q8KuAAtoF-HoK2eluRMg'
+      ),
+    },
   };
   public PUBLIC_HOST: any = {
     hostname: 'broker.hivemq.com',
@@ -38,15 +45,27 @@ export class DisplayMqttMessageComponent{
     port: 8884,
   };
   client?: MqttClient;
-  constructor(private http: HttpClient) {
+  workerURL: SafeResourceUrl;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
+    this.workerURL = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://' + this.MQTT_SERVICE_OPTIONS.hostname + '/mqtt'
+    );
     // certificate issue
-    this.http.get(`https://${this.MQTT_SERVICE_OPTIONS.hostname}/mqtt`);
+    // this.http.get(`https://${this.MQTT_SERVICE_OPTIONS.hostname}/mqtt`);
+  }
+  ngOnInit() {
+    // var ctx = window.open(`https://${this.MQTT_SERVICE_OPTIONS.hostname}/mqtt`);
+    // ctx?.close();
   }
   async connectMqtt() {
     if (!this.MQTT_SERVICE_OPTIONS.properties.authenticationData) return;
     try {
       this.client = await mqtt.connectAsync(this.MQTT_SERVICE_OPTIONS);
       this.message = 'connected';
+      this.client.subscribeAsync('angularmqttdemo/topic1');
+      this.client.on('message', (topic, message) =>
+        console.log(topic, message.toString())
+      );
       console.log(this.client.connected);
     } catch (e) {
       console.log(e);
